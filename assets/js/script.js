9ad459842c2742662c7fa2865256b55d2f39ab85
Paddy Ward <gingerpad@gmail.com>
2017-07-12 08:52:42 +0100

    added BG filters to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,43 +8,60 @@ $(function(){
 			me.PopulatePeople();
 
 			me.DisplayPeople();
+
+			$('.filter').on('click', function(e) {
+				e.preventDefault();
+
+				$('.filter').removeClass('selected');
+				$(this).addClass('selected');
+
+				var filterOption = $(this).attr('data-filter');
+
+				if(filterOption != '') {
+					$('.person').hide();
+
+					$('.person[data-group="' + filterOption + '"]').show()
+				} else {
+					$('.person').show();
+				}
+			});
 		}
 
 		, PopulatePeople: function() {
 			var me = this;
 
 			me.people = [
-				{ name: '',	IGN: 'GAME TIME', location: 'USA', timezone: 'US/Pacific' },
-				{ name: 'Ray',	IGN: 'Rekon19', location: '??, USA', timezone: 'US/Mountain' },
-				{ name: 'Elan',	IGN: 'es128', location: 'Miami, USA', timezone: 'US/Eastern' },
-				{ name: '??', 	IGN: 'evolkill', location: '??, USA', timezone: 'US/Eastern' },
-				{ name: '??',	IGN: 'jlewis8227', location: 'Kentucky, USA', timezone: 'US/Eastern' },
-				{ name: 'Noel',	IGN: 'nellob', location: 'Washington, D.C., USA', timezone: 'US/Eastern' },
-				{ name: 'Nate',	IGN: 'Thatguy345', location: 'New York, USA', timezone: 'US/Eastern' },
-				{ name: '??',	IGN: 'Light Magicians', location: 'Trinidad', timezone: 'America/Port_of_Spain' },
-				{ name: 'Andy', 	IGN: 'Card64', location: 'Ipswich, UK', timezone: 'Europe/London' },
-				{ name: '??', 	IGN: 'Frodo2801', location: '??', timezone: 'Europe/London' },
-				{ name: 'Paddy', 	IGN: 'Gingerpad', location: 'Nottingham, UK', timezone: 'Europe/London' },
-				{ name: '??', 	IGN: 'judgey43', location: 'Essex, UK', timezone: 'Europe/London' },
-				{ name: '??', 	IGN: 'Karolis91', location: 'London, UK', timezone: 'Europe/London' },
-				{ name: 'Kaylee', 	IGN: 'kaylee88', location: 'Nottingham, UK', timezone: 'Europe/London' },
-				{ name: 'Matt', 	IGN: 'Morton666', location: 'Nottingham, UK', timezone: 'Europe/London' },
-				{ name: 'Simon', 	IGN: 'SimonTheGoat', location: 'Derby, UK', timezone: 'Europe/London' },
-				{ name: 'Gary', 	IGN: 'Smivverz', location: 'Ipswich, UK', timezone: 'Europe/London' },
-				{ name: '??', 	IGN: 'Spartan-AROO', location: 'UK', timezone: 'Europe/London' },
-				{ name: '??', 	IGN: 'Stuart36', location: 'UK', timezone: 'Europe/London' },
-				{ name: '??', 	IGN: 'The Slimline', location: 'UK', timezone: 'Europe/London' },
-				{ name: '??', 	IGN: 'Toc.', location: 'Scotland, UK', timezone: 'Europe/London' },
-				{ name: 'Stephen', 	IGN: 'Stephen Brady', location: 'Dublin, Ireland', timezone: 'Europe/Dublin' },
-				{ name: '??', 	IGN: 'E.S.3', location: 'Albania', timezone: 'Europe/Tirane' },
-				{ name: '??', 	IGN: 'Mister_K', location: 'Belgium', timezone: 'Europe/Brussels' },
-				{ name: '??', 	IGN: 'Holm_1924', location: 'Denmark', timezone: 'Europe/Copenhagen' },
-				{ name: '??', 	IGN: 'SamiTheGreat81', location: 'Finland', timezone: 'Europe/Helsinki' },
-				{ name: '??', 	IGN: 'Dearh Avenger', location: 'Norway', timezone: 'Europe/Oslo' },
-				{ name: 'Casper', 	IGN: 'Sathanas', location: 'Sweden', timezone: 'Europe/Stockholm' },
-				{ name: 'Maheshin', 	IGN: 'KILLER_PLAYER', location: 'South Africa', timezone: 'Africa/Johannesburg' },
-				{ name: 'Hiep', 	IGN: 'Rocky Cop', location: 'Singapore', timezone: 'Singapore' },
-				{ name: '??', 	IGN: 'Virgin69', location: 'Philippines', timezone: 'Asia/Manila' }
+				{ name: '',	IGN: 'KABAM TIME', location: 'USA', timezone: 'US/Pacific', group: 0 },
+				{ name: 'Ray',	IGN: 'Rekon19', location: '??, USA', timezone: 'US/Mountain', group: 2 },
+				{ name: 'Elan',	IGN: 'es128', location: 'Miami, USA', timezone: 'US/Eastern', group: 2 },
+				{ name: '??', 	IGN: 'evolkill', location: '??, USA', timezone: 'US/Eastern', group: 1 },
+				{ name: '??',	IGN: 'jlewis8227', location: 'Kentucky, USA', timezone: 'US/Eastern', group: 2 },
+				{ name: 'Noel',	IGN: 'nellob', location: 'Washington, D.C., USA', timezone: 'US/Eastern', group: 2 },
+				{ name: 'Nate',	IGN: 'Thatguy345', location: 'New York, USA', timezone: 'US/Eastern', group: 3 },
+				{ name: '??',	IGN: 'Light Magicians', location: 'Trinidad', timezone: 'America/Port_of_Spain', group: 3 },
+				{ name: 'Andy', 	IGN: 'Card64', location: 'Ipswich, UK', timezone: 'Europe/London', group: 1 },
+				{ name: '??', 	IGN: 'Frodo2801', location: '??', timezone: 'Europe/London', group: 3 },
+				{ name: 'Paddy', 	IGN: 'Gingerpad', location: 'Nottingham, UK', timezone: 'Europe/London', group: 2 },
+				{ name: '??', 	IGN: 'judgey43', location: 'Essex, UK', timezone: 'Europe/London', group: 3 },
+				{ name: '??', 	IGN: 'Karolis91', location: 'London, UK', timezone: 'Europe/London', group: 1 },
+				{ name: 'Kaylee', 	IGN: 'kaylee88', location: 'Nottingham, UK', timezone: 'Europe/London', group: 1 },
+				{ name: 'Matt', 	IGN: 'Morton666', location: 'Nottingham, UK', timezone: 'Europe/London', group: 3 },
+				{ name: 'Simon', 	IGN: 'SimonTheGoat', location: 'Derby, UK', timezone: 'Europe/London', group: 3 },
+				{ name: 'Gary', 	IGN: 'Smivverz', location: 'Ipswich, UK', timezone: 'Europe/London', group: 1 },
+				{ name: '??', 	IGN: 'Spartan-AROO', location: 'UK', timezone: 'Europe/London', group: 1 },
+				{ name: '??', 	IGN: 'Stuart36', location: 'UK', timezone: 'Europe/London', group: 1 },
+				{ name: '??', 	IGN: 'The Slimline', location: 'UK', timezone: 'Europe/London', group: 3 },
+				{ name: '??', 	IGN: 'Toc.', location: 'Scotland, UK', timezone: 'Europe/London', group: 1 },
+				{ name: 'Stephen', 	IGN: 'Stephen Brady', location: 'Dublin, Ireland', timezone: 'Europe/Dublin', group: 3 },
+				{ name: '??', 	IGN: 'E.S.3', location: 'Albania', timezone: 'Europe/Tirane', group: 1 },
+				{ name: '??', 	IGN: 'Mister_K', location: 'Belgium', timezone: 'Europe/Brussels', group: 3 },
+				{ name: '??', 	IGN: 'Holm_1924', location: 'Denmark', timezone: 'Europe/Copenhagen', group: 2 },
+				{ name: '??', 	IGN: 'SamiTheGreat81', location: 'Finland', timezone: 'Europe/Helsinki', group: 2 },
+				{ name: '??', 	IGN: 'Dearh Avenger', location: 'Norway', timezone: 'Europe/Oslo', group: 2 },
+				{ name: 'Casper', 	IGN: 'Sathanas', location: 'Sweden', timezone: 'Europe/Stockholm', group:2 },
+				{ name: 'Maheshin', 	IGN: 'KILLER_PLAYER', location: 'South Africa', timezone: 'Africa/Johannesburg', group: 2 },
+				{ name: 'Hiep', 	IGN: 'Rocky Cop', location: 'Singapore', timezone: 'Singapore', group: 1 },
+				{ name: '??', 	IGN: 'Virgin69', location: 'Philippines', timezone: 'Asia/Manila', group: 3 }
 				
 			]
 		}
@@ -55,7 +72,7 @@ $(function(){
 			for (var i = 0; i < me.people.length; i++) {
 				var person = me.people[i];
 
-				var container = $('<div class="person" data-timezone="' + person.timezone + '"></div>');
+				var container = $('<div class="person" data-timezone="' + person.timezone + '" data-group="' + person.group + '"></div>');
 
 				var detailsContainer = $('<div class="details"></div>');
 
